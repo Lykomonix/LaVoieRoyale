ae806954040120546026729ed783cc7bfa804b53
h.hartz <henri.hartz@etu.unistra.fr>
2023-12-18 22:22:10 +0100

    ajout des scenes de fin de jeu

--- Game/script/laVoieRoyale.gd
+++ Game/script/laVoieRoyale.gd
@@ -20,3 +20,13 @@ func Accueil():
 	window.queue_free()
 	window = preload("res://scene/windowStart.tscn").instantiate()
 	add_child(window)
+
+func gagne():
+	window.queue_free()
+	window = preload("res://scene/windowWin.tscn").instantiate()
+	add_child(window)
+	
+func perdu():
+	window.queue_free()
+	window = preload("res://scene/windowLose.tscn").instantiate()
+	add_child(window)
