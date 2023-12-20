99ea19410943f8de87e15217ce31904f632004d1
Simon Guilloret <simon.guilloret@etu.unistra.fr>
2023-12-20 15:15:35 +0100

    Nouvelles bulles de dialogue

--- Game/Dialogue.gd
+++ Game/Dialogue.gd
@@ -11,19 +11,19 @@ var d_active = false
 func _ready():
 	$NinePatchRect.visible = false
 
-func start():
+func start(index):
 	if d_active:
 		return
 	d_active = true
 	$NinePatchRect.visible = true
-	dialogue = load_dialogue()
+	dialogue = load_dialogue(index)
 	current_dialogue_id = -1
 	next_script()
 	
-func load_dialogue():
+func load_dialogue(index):
 	var file = FileAccess.open(dialogue_path,FileAccess.READ)
 	var content = JSON.parse_string(file.get_as_text())
-	return content
+	return content[index]
 	
 func _input(event):
 	if !d_active:
@@ -38,8 +38,8 @@ func next_script():
 	current_dialogue_id += 1
 	if current_dialogue_id >= len(dialogue):
 		$NinePatchRect.visible = false
-		return 
 		emit_signal("dialogue_finished")
+		return
 		
 	$NinePatchRect/Name.text = dialogue[current_dialogue_id]['name']
 	$NinePatchRect/Text.text = dialogue[current_dialogue_id]['text']
