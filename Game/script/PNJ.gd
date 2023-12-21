5b7c62cf2d24625b838d4fbf040f6c653d796592
h.hartz <henri.hartz@etu.unistra.fr>
2023-12-21 20:43:44 +0100

    le notaire

--- Game/script/PNJ.gd
+++ Game/script/PNJ.gd
@@ -6,14 +6,13 @@ var player
 var player_in_chat_zone = false
 var to_start = false
 var rng = RandomNumberGenerator.new()
-var type = 0
+var type
 
 var dialogue_index = 0
 
 func _ready():
 	$Dialogue.dialogue_path = dialogue_path
 	
-
 func _process(_delta):
 	if Input.is_action_just_pressed("ui_accept") and player_in_chat_zone:
 		to_start=true
@@ -25,11 +24,12 @@ func _on_dialogue_body_entered(body):
 	if body.has_method("player"):
 		player = body
 		player_in_chat_zone = true
-		if(type != 0):
+		if type != 0:
 			$sprQuest.frame = 1
 
 
 func _on_dialogue_body_exited(body):
+	print(type)
 	if body.has_method("player"):
 		player_in_chat_zone = false
 		if type == 1:
@@ -43,7 +43,7 @@ func recompense():
 	else:
 		get_parent().ajouteArgent()
 
-func set_type(type = 0):
+func set_type(type):
 	self.type = type
 	if type == 0:
 		$sprQuest.visible = false
--- Game/script/anime_player.gd
+++ Game/script/anime_player.gd
@@ -2,28 +2,31 @@ extends CharacterBody2D
 
 @export var speed = 150
 var sprite
+var move
 
 func _ready():
 	sprite = load("res://images/persoBase.png")
 	$sprPerso.set_texture(sprite)
+	move = true
 	
 func _process(delta):
-	velocity = Vector2()
-	if Input.is_action_pressed("ui_up"):
-		velocity.y -= speed
-		$animation.play("up")
-	elif Input.is_action_pressed("ui_down"):
-		velocity.y += speed
-		$animation.play("down")
-	elif Input.is_action_pressed("ui_right"):
-		velocity.x += speed
-		$animation.play("right")
-	elif Input.is_action_pressed("ui_left"):
-		velocity.x -= speed
-		$animation.play("left")
-	else:
-		$animation.play("stay")
-	move_and_slide()
+	if move:
+		velocity = Vector2()
+		if Input.is_action_pressed("ui_up"):
+			velocity.y -= speed
+			$animation.play("up")
+		elif Input.is_action_pressed("ui_down"):
+			velocity.y += speed
+			$animation.play("down")
+		elif Input.is_action_pressed("ui_right"):
+			velocity.x += speed
+			$animation.play("right")
+		elif Input.is_action_pressed("ui_left"):
+			velocity.x -= speed
+			$animation.play("left")
+		else:
+			$animation.play("stay")
+		move_and_slide()
 
 func player():
 	pass
