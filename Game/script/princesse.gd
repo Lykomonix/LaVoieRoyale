53f1e97f615357e9745fdaf88990dd450a30e135
h.hartz <henri.hartz@etu.unistra.fr>
2023-12-22 10:29:36 +0100

    fix princesse

--- Game/script/princesse.gd
+++ Game/script/princesse.gd
@@ -2,31 +2,23 @@ extends Node2D
 
 var state = 0
 
-var dialogue_path = "res://dialogue/maire.json"
+var dialogue_path = "res://dialogue/princesse.json"
 
 func _ready():
-	$PNJ/sprPNJ.frame = 9
-	$PNJ.set_type(2)
+	$PNJ/sprPNJ.frame = 1
+	$PNJ.set_type(0)
 	$PNJ/Dialogue.dialogue_path = dialogue_path
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
+	
 func _process(delta):
-	var inventaire = get_parent().inventaire
-	
-	if inventaire.size() > 3:
-		state = 1
-	
 	$PNJ.dialogue_index=state
 	if Input.is_action_just_pressed("ui_accept") and $PNJ.player_in_chat_zone:
+		print("state : ",state)
 		if state == 0:
 			print("début quête")
 			$PNJ/Dialogue.next_script()
-			state-=1
 		elif state == 1:
 			$PNJ/Dialogue.next_script()
 			print("va faire la quete")
-			get_parent().ajouteItem()
+			state +=1
 		else:
 			$PNJ/Dialogue.next_script()
-			print("Merci pour les clients fidèles")
-
